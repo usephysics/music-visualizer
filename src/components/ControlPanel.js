c2b53d118b6b004e9e0e6f7c21d830134b141ae6
ayubh <ayubh@mcmaster.ca>
2020-06-22 01:29:55 -0400

    Implemented early version of visualization, currently draws over control panel (will change soon)

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import Slider from 'react-slick';
 import BgChoice from './BgChoice.js';
+import Visualizer from './Visualizer.js';
+import axios from 'axios';
 import '../css/ControlPanel.css';
 
 export default class ControlPanel extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {uploaded: false}
     }
 
     upload = () => {
+        //let payload = {headers: {'Content-Type': 'multipart/form-data'}, 
+        //               data: {'file' : document.getElementById("file").files[0]}};
+        //console.log(payload);
+        //axios.post("http://localhost:3000/upload", payload, {}).then(res => console.log(res)).catch(rej => console.log(rej));
         document.getElementById("file-name").innerHTML = document.getElementById("file").files[0].name;
+        this.setState({uploaded: true});
     }
 
     render() {
@@ -19,28 +27,32 @@ export default class ControlPanel extends React.Component {
             slidesToShow: 3,
             slidesToScroll: 3,
           };
-        return (
-            <div className="card control-panel">
-                <div className="card-body text-center">
-                    <div className="panel-text mb-2">
-                        Upload a song
-                    </div>
-                    <input type="file" name="file" id="file" accept="audio/*" onChange={this.upload}/>
-                    <label for="file">Upload</label>
-                    <p id="file-name"></p>
-                    <div className="panel-text mt-2 mb-3">
-                        Background and effects
-                    </div>
-                    <div className="carousel mx-auto">
-                        <Slider {...settings}>
-                            {Array(6).fill(0).map((e,i)=>i+1).map(num =>
-                                <BgChoice num={num} active={this.props.active} 
-                                changeBackground={this.props.changeBackground}/>
-                            )}
-                        </Slider>
+        if (this.state.uploaded){
+            return(<Visualizer></Visualizer>);
+        }else{
+            return (
+                <div className="card control-panel">
+                    <div className="card-body text-center">
+                        <div className="panel-text mb-2">
+                            Upload a song
+                        </div>
+                        <input type="file" name="file" id="file" accept="audio/*" onChange={this.upload}/>
+                        <label for="file">Upload</label>
+                        <p id="file-name"></p>
+                        <div className="panel-text mt-2 mb-3">
+                            Background and effects
+                        </div>
+                        <div className="carousel mx-auto">
+                            <Slider {...settings}>
+                                {Array(6).fill(0).map((e,i)=>i+1).map(num =>
+                                    <BgChoice num={num} active={this.props.active} 
+                                    changeBackground={this.props.changeBackground}/>
+                                )}
+                            </Slider>
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 }
---
+++ src/components/Visualizer.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ControlPanel from './ControlPanel.js';
+import Bars from './Bars/Bars.js';
+import Bar from './Bars/Bar.js';
+
+export default class Visualizer extends React.Component {
+    constructor(props) {
+        super(props);
+        const freqRange = [20, 60, 250, 500, 2000, 4000, 6000, 20000]
+        const audioCtx = new AudioContext();
+        const song = new Audio();
+        song.src = '../files/T2.mp3';
+        song.play();
+        const analyserNode = audioCtx.createAnalyser();
+        analyserNode.fftSize = 2048;
+        const dataArr = new Uint8Array(analyserNode.frequencyBinCount);
+        //call bars with this
+        const audioSourceNode = audioCtx.createMediaElementSource(song);
+        audioSourceNode.connect(analyserNode);
+        analyserNode.connect(audioCtx.destination);
+        this.state = {analyser: analyserNode, dataArray: dataArr, color: "255,255,255"}
+    }
+
+    updateDA = () => {
+        let analyser = this.state.analyser;
+        let dataArr = this.state.dataArray;
+        analyser.getByteFrequencyData(dataArr);
+        console.log(dataArr);
+        this.setState({analyser: analyser, dataArray: dataArr, color: 
+            dataArr.subarray(4,8).reduce((total, next) => total += next)/4 > 210 ? "150,255,255" : "255,255,255"});
+    }
+
+    componentDidMount(){
+        setInterval(this.updateDA, 25);
+    }
+
+    render() {
+        return(
+            <div className="bars">
+                <div className="hidden-bar"></div>
+                {[...Array(32)].map((e, i) => {
+                    return <Bar minHeight={this.state.dataArray[i*i]/25.6} varHeight={0} speed={50} rgb={this.state.color}/>
+                })}
+            </div>
+        );
+    }
+}
