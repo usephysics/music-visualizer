f4213aab0e3b5b18a7f13278419268c0f7a7f8f2
sunnybh <sunnynbhatt@gmail.com>
2020-08-08 12:59:49 -0400

    Audio player time remaining text

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -9,13 +9,15 @@ export default class AudioPlayer extends React.Component {
     constructor(props) {
         super(props);
         this.state = { playing: true };
-        this.audio = this.props.audio;
+        this.lastTimeUpdate = 0;
     };
 
     componentDidMount() {
-        this.audio.addEventListener("timeupdate", () => {
+        this.props.audio.addEventListener("timeupdate", () => {
+            if (Date.now() - this.lastTimeUpdate < 300) return;
             if (this.timeline == null) return;
-            let ratio = this.audio.currentTime / this.audio.duration;
+            this.lasteTimeUpdate = Date.now();
+            let ratio = this.props.audio.currentTime / this.props.audio.duration;
             let position = (this.timeline.offsetWidth * ratio) + this.timeline.offsetLeft;
             this.positionHandle(position);
         });
@@ -39,8 +41,8 @@ export default class AudioPlayer extends React.Component {
     mouseMove = (e) => {
         if (this.timeline == null || this.handle == null) return;
         this.positionHandle(e.pageX);
-        this.audio.currentTime = ((e.pageX - this.timeline.offsetLeft) / this.timeline.offsetWidth) * this.audio.duration;
-        this.audio.play();
+        this.props.audio.currentTime = ((e.pageX - this.timeline.offsetLeft) / this.timeline.offsetWidth) * this.props.audio.duration;
+        this.props.audio.play();
         this.setState({ playing: true });
     };
 
@@ -57,20 +59,24 @@ export default class AudioPlayer extends React.Component {
     play = () => {
         if (this.state.playing) {
             this.setState({ playing: false });
-            this.audio.pause();
+            this.props.audio.pause();
         } else {
             this.setState({ playing: true });
-            this.audio.play();
+            this.props.audio.play();
         }
     };
 
     render() {
+        let timeLeft = Math.round(this.props.audio.duration - this.props.audio.currentTime);
+        let seconds = (timeLeft % 60);
+        let minutes = (timeLeft - seconds) / 60
         return <div className="audio-player">
             <div className="play-pause-btn" onClick={this.play}>{this.state.playing ? <FontAwesomeIcon icon={faPause} /> : <FontAwesomeIcon icon={faPlay} />}</div>
             <div className="timeline" onClick={this.mouseMove} ref={(timeline) => { this.timeline = timeline }}>
                 <div className="line"></div>
                 <div className="handle" onMouseDown={this.mouseDown} ref={(handle) => { this.handle = handle }} />
             </div>
+            <div className="timeline-time">-{minutes + ":" + seconds.toString().padStart(2, 0)}</div>
         </div>
     }
 }
